de417533d497dd195697d967805ebd642411e131
kpubben <kpubben@silverspringenergy.com>
2019-05-17 12:06:06 -0600

    hi

---
+++ logic/getCapabilities.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+import urllib.request as urllib
+import os
+
+def open_wmts():
+    urllib.urlretrieve('http://192.168.1.232:8080/geoserver/BurntLake/gwc/service/wmts?REQUEST=GetCapabilities',
+                               'wmts.xml')
+    with open('wmts.xml', 'r') as fh:
+        tree = ET.parse(fh)
+        for name in tree.iter('{http://www.opengis.net/ows/1.1}Title'):
+            print (name.text.x)
+
+class class_variable():
+    x = []
+
+open_wmts()
+
+os.remove('wmts.xml')
